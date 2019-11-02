924b1d89c03eaf43a47267f52991cc4d380dde75
Geshna Aggarwal <geshna.aggarwal@duke.edu>
2019-11-02 18:10:16 -0400

    progress

---
+++ js/progressBar.js
@@ -0,0 +1,24 @@
+var char1sum = 1.0;
+var goal = 5.0;
+var percentChar1 = char1sum/goal;
+
+
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+
