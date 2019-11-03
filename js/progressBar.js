a6212300faf2ac2e6723f43b07d71417fe4eb7d1
Geshna Aggarwal <geshna.aggarwal@duke.edu>
2019-11-02 21:59:54 -0400

    variable progress

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,7 +1,8 @@
 var char1sum = 1.0;
 var goal = 5.0;
-var percentChar1 = char1sum/goal;
+var percentChar1 = 20;
 
+document.getElementById('meter1').style.width= percentChar1 +"px";
 
 var i = 0;
 function move() {
