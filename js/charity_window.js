9855912fa9875f4e366f39a3de75d7513b41ef51
Raymond Lin <raymond.lin@duke.edu>
2019-11-03 06:49:44 -0500

    charity site closes when charity is clicked

--- js/charity_window.js
+++ js/charity_window.js
@@ -1,27 +1,30 @@
 //var counts = -1;
-var charities = [
+const education = [
     {
         name: 'UNICEF',
         description: 'UNICEF works in over 190 countries and territories to save children’s lives, to defend their rights, and to help them fulfil their potential, from early childhood through adolescence. And we never give up.',
-        category: 'education',
         url: 'https://www.icpcn.org/wp-content/uploads/2013/08/Unicef-Logo.png'
     },
     {
         name: 'ProLiteracy',
         description: 'ProLiteracy, the largest adult literacy and basic education membership organization in the nation, believes that a safer, stronger, and more sustainable society starts with an educated adult population. For more than 60 years, ProLiteracy has been working across the globe to change lives and communities through the power of literacy.',
-        category: 'education',
         url: 'https://cdn1.imggmi.com/uploads/2019/11/3/31cdb5d508f07e3766c8c95d9caab657-full.png'
     },
     {
         name: 'Room to Read',
         description: 'Room to Read seeks to transform the lives of millions of children in low-income communities by focusing on literacy and gender equality in education.',
-        category: 'education',
         url: 'https://cdn1.imggmi.com/uploads/2019/11/3/6bb6e84d6e3aec39476cca6d35044eca-full.png',
+    },
+    {
+        name:'Pencils of Promise',
+        description:'We believe every child should have access to quality education. We build schools and provide quality educational programming to increase literacy rates in Ghana, Guatemala, Laos and Nicaragua.',
+        url: 'https://cdn1.imggmi.com/uploads/2019/11/3/868a7bab444f22e9f9c13d36f30bf6fb-full.png'
     }
-]
+];
+
 function startupCharity(){
+    var charities = education;
     document.getElementById('charity_logo').src = chrome.extension.getURL('assets/penny.png');
-    // var obj = JSON.parse("{ 'name':'John', 'age':30, 'city':'New York}');
     var hover1 = document.getElementById('hover1');
     var hover2 = document.getElementById('hover2');
     var hover3 = document.getElementById('hover3');
@@ -54,6 +57,9 @@ function startupCharity(){
 
     // console.log();
 
+    document.getElementById("firstButton").addEventListener('click', closeCharityWindow);
+    document.getElementById("secondButton").addEventListener('click', closeCharityWindow);
+    document.getElementById("thirdButton").addEventListener('click', closeCharityWindow);
     addMoney();
 }
 
@@ -102,4 +108,10 @@ function newTotal(){
             })
         });
     }
+}
+function closeCharityWindow(){
+    document.getElementById('charity-window').style.display = 'none';
+    document.getElementById('charity-overlay').style.display = 'none';
+    document.getElementById('buy-window').style.display = 'none';
+    document.getElementById('buy-overlay').style.display = 'none';
 }
