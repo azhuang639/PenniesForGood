bf9628d6d97b6f843c5ed81ec3d1b0fc0482259e
Alex Zhuang <azhuang639@gmail.com>
2019-11-03 00:49:50 -0400

    yeet

--- js/extension_window.js
+++ js/extension_window.js
@@ -32,10 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function newPopup(url) {
-    popupWindow = window.open(url,'popUpWindow',
-        'height=600,width=1100,left=10,top=10,' +
-        'resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,' +
-        'location=no,directories=no,status=yes')
+    chrome.tabs.create({url: url})
 }
 
 function reverse(element) {
